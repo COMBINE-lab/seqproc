e19ceae712f9ebbd2fc48fc6e8cc908767789c40
Noah Cape <noahcape@gmail.com>
2023-07-18 14:10:52 -0400

    compile functions

--- README.md
+++ README.md
@@ -4,20 +4,25 @@ Areas which need work with `seqproc`
 - [x] Parse number as `usize`
 - [ ] Validate bounds and variables for functions (decide if functions are `padTO` or `padBY`)
 - [x] Execute transformations, tie it to `collect_fastq`
-- [ ] Execute stack 
+- [x] Execute stack 
 	- [x] Propagate fixed size and ranged size to execute stack
 - [x] Store stack in the rev direction, insert and remove `O(1)`
 - [x] Force parse transformation if `->` is present
 - [ ] Handle ANTISEQUENCE errors
 - [ ] Better error checking FGDL
-- [ ] Map should take a sequence or label as a fallback
+- [x] Map should take a sequence or label as a fallback
 - [x] Test order of functions
 
 #todo 
 ANTISEQUENCE functions to add:
 - [x] normalize
 - [x] pad
-- [ ] trucate
-- [ ] reverse
-- [ ] reverse compliment
-- [ ] map
+	- [ ] left and right
+	- [ ] specify character
+- [x] trucate
+	- [ ] left and right
+	- [ ] specify character
+- [x] reverse
+- [x] reverse compliment
+- [x] map
+- [x] map with mismatch
---
+++ src/geometry/compile/functions.rs
@@ -0,0 +1,124 @@
+/*
+   Compile functions read from parser
+   This is specifically for map as this point
+   but in the future when functions can accept
+   expressions with label references a seperate
+   place for this to happen will be useful
+*/
+
+use std::ops::Deref;
+
+use crate::parser::{Expr, Function, Spanned};
+
+use super::utils::{validate_expr, Error, GeometryMeta, GeometryPiece};
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum CompiledFunction {
+    Reverse,
+    ReverseComp,
+    Truncate(usize),
+    Remove,
+    Pad(usize),
+    Normalize,
+    Map(String, Vec<Spanned<CompiledFunction>>),
+    MapWithMismatch(String, Vec<Spanned<CompiledFunction>>, usize),
+    Hamming(usize),
+}
+
+pub fn compile_fn(
+    fn_: Spanned<Function>,
+    parent_expr: Spanned<Expr>,
+) -> Result<Spanned<CompiledFunction>, Error> {
+    let (fn_, span) = fn_;
+    let (parent_expr, expr_span) = parent_expr;
+    let comp_fn = match fn_ {
+        Function::Reverse => CompiledFunction::Reverse,
+        Function::ReverseComp => CompiledFunction::ReverseComp,
+        Function::Truncate(n) => CompiledFunction::Truncate(n),
+        Function::Remove => CompiledFunction::Remove,
+        Function::Pad(n) => CompiledFunction::Pad(n),
+        Function::Normalize => CompiledFunction::Normalize,
+        Function::MapWithMismatch(path, expr, mismatch) => CompiledFunction::MapWithMismatch(
+            path,
+            compile_inner_expr(expr.deref().clone(), (parent_expr, expr_span))?,
+            mismatch,
+        ),
+        Function::Map(path, expr) => CompiledFunction::Map(
+            path,
+            compile_inner_expr(expr.deref().clone(), (parent_expr, expr_span))?,
+        ),
+        Function::Hamming(n) => CompiledFunction::Hamming(n),
+    };
+
+    Ok((comp_fn, span))
+}
+
+fn compile_inner_expr(
+    expr: Spanned<Expr>,
+    parent_expr: Spanned<Expr>,
+) -> Result<Vec<Spanned<CompiledFunction>>, Error> {
+    // if we are here in a map then the expr passed into the expr should be a geom_piece or labeled geom_piece
+    // either way we can extract the size and type of it
+    let mut expr = expr.clone();
+    let mut stack: Vec<Spanned<CompiledFunction>> = Vec::new();
+
+    loop {
+        match expr.0 {
+            Expr::Self_ => break,
+            Expr::Function(inner_fn, inner_expr) => {
+                expr = inner_expr.deref().clone();
+                let inner_fn = compile_fn(inner_fn.clone(), expr.clone());
+                if inner_fn.is_ok() {
+                    stack.push(inner_fn.ok().unwrap());
+                } else {
+                    return Err(Error {
+                        span: expr.1,
+                        msg: "Invalid function composition".to_string(),
+                    });
+                }
+            }
+            _ => {
+                return Err(Error {
+                    span: expr.1,
+                    msg: "Must refernce only `self` in `map` fallback argument".to_string(),
+                })
+            }
+        }
+    }
+
+    let geom_piece = {
+        let (mut expr, span) = parent_expr;
+        loop {
+            match expr {
+                Expr::LabeledGeomPiece(_, b) => {
+                    let (gp, _) = b.deref();
+                    expr = gp.clone();
+                }
+                Expr::GeomPiece(_, _) => break,
+                Expr::Label(_) => todo!(),
+                _ => todo!(), // throw error
+            };
+        }
+
+        if let Expr::GeomPiece(type_, size) = expr {
+            GeometryPiece {
+                type_,
+                size,
+                label: None,
+            }
+        } else {
+            return Err(Error {
+                span,
+                msg: format!("Expected geometry peice found: {}", expr),
+            });
+        }
+    };
+
+    // check this and return esult from this function
+    validate_expr(GeometryMeta {
+        expr: (geom_piece, expr.1),
+        stack: stack.clone(),
+    })?;
+
+    Ok(stack)
+}
